b809c0cdd64077706bf95bff1e6df48a15b5dfff
Kaique Luz <60513687+KaiqueLuz23@users.noreply.github.com>
2020-11-23 21:30:17 -0300

    Delete contador_letras.py

--- contador_letras.py
+++
@@ -1,9 +0,0 @@
-def contador_letras(lista_palavras):
-    contador = []
-    for x in lista_palavras:
-        quantidade = len(x)
-        contador.append(quantidade)
-    return  contador
-if __name__ == '__main__':
-    lista = ['palavra']
-print(contador_letras(lista))
