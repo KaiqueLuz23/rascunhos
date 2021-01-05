16781a79c5476ede7022f6769eac3fa45202e64b
KaiqueLuz23 <kakaluzrda@gmail.com>
2021-01-05 13:30:15 -0300

    commit js

---
+++ JS/teste/js/main.js
@@ -0,0 +1,73 @@
+/*
+var nome = "Kaique  Luz";
+var idade = 23;
+var frase = 'Flamengo'
+//var idade = 21;
+//alert("Bem vindo " + nome + "sua idade é " + idade);
+alert(nome.replace("Luz", "Emanuel"));
+console.log(nome)
+console.log(idade)
+console.log(frase.replace("Flamengo", "FlaSampa"))*/
+
+/*
+var lista = ["Uva", "Maça", "Banana"];
+
+// adicionar na lista
+//lista.push("Manga");
+// remover da lista
+//lista.pop("Uva");
+
+//console.log(lista[1]);
+// para ver tamanaho
+//console.log(lista.length);
+
+//console.log(lista.reverse);
+//console.log(lista);
+// tranformar array 
+console.log(lista.toString());
+console.log(lista.join(" - "));*/
+
+/*
+var frutas = [{nome:"Maça", cor:["vermelha","verde"]},
+              {nome:"Banana", cor:"amarela"}  
+             ];
+console.log(frutas[0].nome + " " + frutas[0].cor[0]);
+console.log(frutas[1].nome + " " + frutas[0].cor[1]);
+*/
+
+/*
+var idade = prompt("Sua idade: ");
+//var idade = 18;
+if(idade >= 18){
+    alert("Maior");
+}else{
+    alert("menor");
+}*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    //count = count + 1;
+    count++;
+}*/
+
+/*
+var count;
+for(count=0; count <=5; count ++){
+    console.log(count);
+}*/
+
+/*
+
+var d = new Date();
+alert(d.getMonth()+1);
+
+Alert (“Digital Innovation One:” + formacoes)
+*/
+
+function clicou(){
+    document.getElementById("obg").innerHTML = "<b>Obrigado !!</b>";
+    //console.log(document.getElementById("obg"));
+    //alert("Obrigado por clicar !");
+}
