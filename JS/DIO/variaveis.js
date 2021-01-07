0f8af19f11bae2a560126da953845925dd28295e
KaiqueLuz23 <kakaluzrda@gmail.com>
2021-01-07 11:41:03 -0300

    commit js

--- JS/DIO/variaveis.js
+++ JS/DIO/variaveis.js
@@ -9,3 +9,23 @@ console.log('\nArray com as posições separadas pelo demilitador: ', splittedTe
 //Buscar por um substiuir por outro 
 const replaceTexto = 'Kaique Luz'.replace('Luz', 'Emanuel');
 console.log('\nSubstituição: ', replaceTexto );
+
+//Retornar a "fatia" de um valor
+const lastChar = 'Kaique'.slice(-1);
+console.log('\nUltima letra de uma string:  ', lastChar);
+
+
+const myNumber = 12.4032;
+//Transformar numero para string
+const numberAsString = myNumber.toString();
+console.log('Numero transformado em string: ', typeof numberAsString);
+
+//Retornar numero com um numero de casa decimais
+const fixedDoisDecimeias = myNumber.toFixed(2);
+console.log('\nNumero com duas casas decimais: ', fixedDoisDecimeias);
+
+//Transformar uma string em float
+console.log('\nString para float: ', parseFloat('13.22'));
+
+//Transfomar uma string em inteiro
+console.log('\nString para int: ', parseInt(myNumber));
