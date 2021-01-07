bd3850709f45e5ffbea9e68f6c99816e54fec6fa
KaiqueLuz23 <kakaluzrda@gmail.com>
2021-01-07 14:48:58 -0300

    commit js

--- JS/DIO/variaveis.js
+++ JS/DIO/variaveis.js
@@ -29,3 +29,66 @@ console.log('\nString para float: ', parseFloat('13.22'));
 
 //Transfomar uma string em inteiro
 console.log('\nString para int: ', parseInt(myNumber));
+
+
+let user = {
+    name: 'Kaiqque'
+};
+
+//Alterar propriedade de um objeto 
+user.name = 'Kaique';
+user['name'] = 'Kaique Luz';
+
+console.log('\nObjeto: ', user);
+
+//Criando uma propriedade 
+user.idade = '21';
+user.teste = 'a';
+console.log(user);
+//Deletando uma propriedade
+delete user.teste;
+console.log(user);
+
+//Recupear chaves
+console.log(Object.keys(user));
+//Recuperar valores
+console.log(Object.values(user));
+
+//Retornar array de array 
+console.log(Object.entries(user));
+
+// mergear propriedades do objects (não recomen)
+Object.assign(user, {teste2: 'b'});
+console.log(user);
+// mergear propriedades do objects (recomen)
+console.log(Object.assign({}, user, { name: 'Kaique Luz', idade: '21', teste3: 'c' }));
+console.log(user);
+
+//Predefine todas as alterações eem um objeto
+const novoObj = { foo: 'bar'};
+Object.freeze(novoObj);
+
+novoObj.foo = 'teste';
+delete novoObj.foo;
+novoObj.bar = 'foo';
+
+console.log(novoObj);
+
+//Permite apenas troca de valor existente
+
+const carro = {nome: 'Ferrari'};
+Object.seal(carro)
+
+carro.nome = 'Lamborghini';
+delete carro.nome;
+carro.age = '2021';
+
+console.log(carro);
+
+
+//Symbols sãoo unicos 
+
+const sy1 = Symbol('a');
+const sy2 = Symbol('a');
+
+console.log('symbol 1 é igual ao symbol 2 ? ', sy1 === sy2);
